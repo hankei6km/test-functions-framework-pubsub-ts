dbe69047ec3649575d33bd3ef60b5109743a9bcb
hankei6km <gh-hankei6km@outlook.jp>
2023-02-09 07:18:26 +0000

    feat: Return JSON from `chk1` function
    It contained `handleId` temporary.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import * as functions from '@google-cloud/functions-framework'
+import { getReqId } from './lib/reqid.js'
 
 // Register an HTTP function with the Functions Framework
 functions.http('chk1', async (req, res) => {
-  res.send('OK')
+  const info = await getReqId({
+    sheetId: '', // from request.
+    bundleId: '', // from request.
+    password: '', // from secret(ev var)
+    salt: '' // from secret(ev var)
+  })
+  res.json({ handleId: info.handleId })
 })
